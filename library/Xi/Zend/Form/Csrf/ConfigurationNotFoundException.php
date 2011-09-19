5f2af9e5e1bb105258f2dfd6f50b9b6483504839
ezku <eevert.saukkokoski@brainalliance.com>
2011-09-19 13:50:50 +0300

    migrated form classes to xi-zend-form

--- library/Xi/Zend/Form/Csrf/ConfigurationNotFoundException.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace Xi\Zend\Form\Csrf;
-
-class ConfigurationNotFoundException extends \DomainException implements \Xi\Zend\Exception
-{}
--- library/Xi/Zend/Form/Csrf/FeatureDisabledException.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace Xi\Zend\Form\Csrf;
-
-class FeatureDisabledException extends \LogicException implements Xi\Zend\Exception
-{}
